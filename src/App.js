8d2122ec1ff274ecc48cee153ddb79ca810c735b
Sohyunnnn <thgus05061@naver.com>
2023-11-30 02:01:08 +0900

    feat: MainPage 화면 구성 및 배경 추가

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   useEffect(() => {
     setScreenSize();
   });
-  
+
   return (
     
     <BrowserRouter>
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import "./MainPage.css";
+import MainLogo from "../../images/MainLogo.svg";
+import BlackBoard from "../../images/BlackBoard.svg";
+import WallPaper from "../../images/WallPaper.svg";
 
-const MainPage= () => {
+const MainPage = () => {
   return (
     <div className='MainPage'>
-        <h3>hello</h3>
+      <div className='Main'>
+        <div className="imageContainer">
+          <img src={WallPaper} alt="WallPaper" className="overlappingImage" />
+          <img src={BlackBoard} alt="BlackBoard" className="overlappingImage" />
+          <img src={MainLogo} alt="Main Logo" className="overlappingImage"/>
+        </div>
+        <div >
+            <div className='molding'></div>
+            <div className='floor'></div>
+        </div>
+        
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;

8d729ce17a1f026dd760229760881b135d16091f
Sohyunnnn <thgus05061@naver.com>
2024-01-03 23:35:20 +0900

    feat: MainPage 화면 구성 완료

---
+++ src/components/Locker/Locker.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Locker.css";
+
+function Locker() {
+  return (
+    <div className='locker'>
+      <h1 className='groupName'></h1> 
+    </div>
+  )
+}
+
+export default Locker
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,12 +1,43 @@
 import React from "react";
 import "./MainPage.css";
+import Locker from "../../components/Locker/Locker";
+import SmallLogoImg from "../../images/SmallLogo.svg";
+import ProfileImg from "../../images/Profile.svg";
 
 
 const MainPage = () => {
   return (
     <div className="MainPage">
-        <h1>hi</h1>
-        <h1>hi</h1>
+      <div className="Header">
+        <img className="Profile" src={ProfileImg}/>
+        <img className="SmallLogo" src={SmallLogoImg}/>
+      </div>
+      
+      <div className="Locker">
+        <Locker />
+        <Locker />
+        <Locker />
+        <Locker />
+        <Locker />
+        <Locker />
+        <Locker />
+        <Locker />
+        <Locker />
+        </div>
+
+        <div className="EditGroup">
+          <button className="EditButton">
+            추가
+          </button>
+          <button className="EditButton">
+            편집
+          </button>
+          <button className="EditButton">
+            삭제
+          </button>
+          
+        </div>
+        
     </div>
   );
 };

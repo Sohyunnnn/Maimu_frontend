bc0d67c01cbde4646833c32503e2185f138cf9f2
letthem <suyeon3662@naver.com>
2024-02-03 18:09:06 +0900

    Feat: 개별 사물함 페이지 구현

---
+++ src/pages/DetailPage/DetailPage.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import "./DetailPage.css";
+import "../../components/PasteLinkAlert/PasteLinkAlert.css";
+import SmallLogoImg from "../../images/SmallLogo.svg";
+import PasteLink from "../../images/DetailPage/PasteLink.svg";
+import DetailMaimu from "../../components/DetailMaimu/DetailMaimu";
+import { PasteLinkAlert } from "../../components/PasteLinkAlert/PasteLinkAlert";
+
+const DetailPage = () => {
+  const [pasteState, setPasteState] = useState(false);
+
+  return (
+    <div className="DetailPage">
+      <div className="Header">
+        <img className="SmallLogo" alt="" src={SmallLogoImg} />
+        <img
+          className="PasteLink"
+          alt="PasteLink"
+          src={PasteLink}
+          onClick={() => setPasteState(true)}
+        />
+      </div>
+      <div className="GroupName">그룹 이름</div>
+      <div className="DetailMaimu">
+        <DetailMaimu />
+        <DetailMaimu />
+        <DetailMaimu />
+        <DetailMaimu />
+      </div>
+      {pasteState && <PasteLinkAlert setPasteState={setPasteState} />}
+    </div>
+  );
+};
+
+export default DetailPage;

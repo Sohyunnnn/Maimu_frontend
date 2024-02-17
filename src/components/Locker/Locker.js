2b3d312c3849fd2d9baf20f84eb3ced81efa89e4
Sohyunnnn <thgus05061@naver.com>
2024-02-17 23:52:26 +0900

    Feat: 편집, 삭제 버튼 클릭 시 선택할 수 있는 UI 구현

--- src/components/Locker/Locker.js
+++ src/components/Locker/Locker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Locker.css';
 import LockerIcon from '../../images/MainPage/Locker.svg';
 import GreenLockerIcon from '../../images/MainPage/GreenLocker.svg';
@@ -6,6 +6,8 @@ import PinkLockerIcon from '../../images/MainPage/PinkLocker.svg';
 import YellowLockerIcon from '../../images/MainPage/YellowLocker.svg';
 
 const Locker = ({ GroupName, groupColor }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   let lockerIcon;
 
   // 그룹 색상에 따라 적절한 Locker 아이콘을 선택합니다.
@@ -28,6 +30,7 @@ const Locker = ({ GroupName, groupColor }) => {
     <div className='LockerContainer'>
       <img className='LockerIcon' src={lockerIcon} alt='LockerIcon' />
       <div className='GroupName'>{GroupName}</div>
+      {isEditing && <div className='CheckCircle'></div>}
     </div>
   );
 };
